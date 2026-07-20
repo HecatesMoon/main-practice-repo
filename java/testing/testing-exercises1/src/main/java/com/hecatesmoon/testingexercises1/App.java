f1413eb34b1624fc11967b6c194be90ae0fb09c1
Joaquín Araya <88943020+HecatesMoon@users.noreply.github.com>
2026-07-20 18:22:28 -0400

    chore: create new maven project

---
+++ java/testing/testing-exercises1/src/main/java/com/hecatesmoon/testingexercises1/App.java
@@ -0,0 +1,10 @@
+package com.hecatesmoon.testingexercises1;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ java/testing/testing-exercises1/src/main/java/com/hecatesmoon/testingexercises1/utils/TextUtils.java
@@ -0,0 +1,7 @@
+package com.hecatesmoon.testingexercises1.utils;
+
+public class TextUtils {
+    
+    public TextUtils (){}
+
+}
