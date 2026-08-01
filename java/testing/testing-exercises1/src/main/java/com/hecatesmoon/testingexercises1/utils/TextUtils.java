cf32e8f085d556e283f946c216a072706b3dc397
JoacoOrbit <88943020+HecatesMoon@users.noreply.github.com>
2026-08-01 00:38:30 -0400

    feat: add isPalindrome function

--- java/testing/testing-exercises1/src/main/java/com/hecatesmoon/testingexercises1/utils/TextUtils.java
+++ java/testing/testing-exercises1/src/main/java/com/hecatesmoon/testingexercises1/utils/TextUtils.java
@@ -4,4 +4,21 @@ public class TextUtils {
     
     public TextUtils (){}
 
+    public boolean isPalindrome(String word){
+        int start = 0;
+        int end = word.length()-1;
+        int half = end / 2;
+
+        for (int i = 1; i <= half ; i++){
+            if (word.charAt(start) != word.charAt(end)){
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
 }
