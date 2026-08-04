06c3a8213b6179c751e0e9e9189cdb638b67426f
JoacoOrbit <88943020+HecatesMoon@users.noreply.github.com>
2026-08-04 17:12:20 -0400

    feat: add calculateMedian method

--- java/testing/testing-exercises1/src/main/java/com/hecatesmoon/testingexercises1/utils/StatUtils.java
+++ java/testing/testing-exercises1/src/main/java/com/hecatesmoon/testingexercises1/utils/StatUtils.java
@@ -1,5 +1,7 @@
 package com.hecatesmoon.testingexercises1.utils;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,6 +11,26 @@ public class StatUtils {
 
     public Double calculateAverage(List<Integer> numbers){
         return numbers.stream().collect(Collectors.averagingInt(n -> n));
+    }
+
+    public Double calculateMedian(List<Integer> numbers){
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.addAll(numbers);
+
+        list.sort(Comparator.naturalOrder());
+
+        if (list.size() % 2 == 0){
+            Integer value1 = list.get((list.size() / 2)-1);
+            Integer value2 = list.get((list.size() / 2));
+
+            return (Double.valueOf(value1) + Double.valueOf(value2)) / 2;
+        } else {
+            Integer result = list.get((list.size() / 2));
+
+            return Double.valueOf(result);
+        }
     }
 
 }
