3c40ef7ffb40addd656e85174cc549eae4b52c38
JoacoOrbit <88943020+HecatesMoon@users.noreply.github.com>
2026-08-08 23:09:04 -0400

    feat: add reduceStock method

--- java/testing/testing-exercises1/src/main/java/com/hecatesmoon/testingexercises1/inventory/SimpleInventory.java
+++ java/testing/testing-exercises1/src/main/java/com/hecatesmoon/testingexercises1/inventory/SimpleInventory.java
@@ -3,6 +3,11 @@ package com.hecatesmoon.testingexercises1.inventory;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.naming.InsufficientResourcesException;
+
+import com.hecatesmoon.testingexercises1.exceptions.InsufficientStockException;
+import com.hecatesmoon.testingexercises1.exceptions.ProductNotFoundException;
+
 public class SimpleInventory {
     
     private Map<String, Integer> inventory = new HashMap<>();
@@ -19,6 +24,20 @@ public class SimpleInventory {
 
     public void editProductQty(String product, int qty){
         inventory.put(product, qty);
+    }
+
+    public void reduceStock(String product, int qty){
+        if (!inventory.containsKey(product)){
+            throw new ProductNotFoundException("this product does not exist: " + product);
+        }
+
+        if (qty > inventory.get(product)){
+            throw new InsufficientStockException("you are trying to reduce more stock than available " + inventory.get(product));
+        }
+
+        int newValue = inventory.get(product) - qty;
+
+        inventory.put(product, newValue);
     }
 
 
