8d6426169f6e806c9ea01bfd56f99222994d562e
JoacoOrbit <88943020+HecatesMoon@users.noreply.github.com>
2026-08-12 21:49:35 -0400

    test: add tests for CountVocals from TextUtils

--- java/testing/testing-exercises1/src/test/java/com/hecatesmoon/testingexercises1/utils/TextUtilsTest.java
+++ java/testing/testing-exercises1/src/test/java/com/hecatesmoon/testingexercises1/utils/TextUtilsTest.java
@@ -59,4 +59,82 @@ public class TextUtilsTest {
         Assertions.assertFalse(result);
     }
 
+
+    @Test
+    public void testCountVocals_FourVocalsWord(){
+        int result = textUtils.countVocals("acetato");
+
+        Assertions.assertEquals(4, result);
+    }
+
+    @Test
+    public void testCountVocals_NoVocalsWord(){
+        int result = textUtils.countVocals("rythm");
+
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    public void testCountVocals_OnlySpaces(){
+        int result = textUtils.countVocals("       ");
+
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    public void testCountVocals_EmptyString(){
+        int result = textUtils.countVocals("");
+
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    public void testCountVocals_OnlyCaps(){
+        int result = textUtils.countVocals("MADERA");
+
+        Assertions.assertEquals(3, result);
+    }
+
+    @Test
+    public void testCountVocals_RandomSpaces(){
+        int result = textUtils.countVocals("  l ob ot o mi  a   ");
+
+        Assertions.assertEquals(5, result);
+    }
+
+    @Test
+    public void testCountVocals_BorderSpaces(){
+        int result = textUtils.countVocals("  hamburguesa   ");
+
+        Assertions.assertEquals(5, result);
+    }
+     
+    @Test
+    public void testCountVocals_WordWithAccent(){
+        int result = textUtils.countVocals("azúcar");
+
+        Assertions.assertEquals(3, result);
+    }
+     
+    @Test
+    public void testCountVocals_WordWithAccentAndCaps(){
+        int result = textUtils.countVocals("Algodón");
+
+        Assertions.assertEquals(3, result);
+    }
+     
+    @Test
+    public void testCountVocals_OnlyVocals(){
+        int result = textUtils.countVocals("aaeeioua");
+
+        Assertions.assertEquals(8, result);
+    }
+
+    @Test
+    public void testCountVocals_OnlyVocalsWithAccents(){
+        int result = textUtils.countVocals("áééííóóííú");
+
+        Assertions.assertEquals(10, result);
+    }
+
 }
