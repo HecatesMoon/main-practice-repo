d731b018316566ace4676126c875db47870bd478
JoacoOrbit <88943020+HecatesMoon@users.noreply.github.com>
2026-08-14 23:49:02 -0400

    test: add simple tests for calculateAverage from StatUtils

---
+++ java/testing/testing-exercises1/src/test/java/com/hecatesmoon/testingexercises1/utils/StatUtilsTest.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatUtilsTest {
+
+    private final StatUtils statUtils = new StatUtils();
+
+    public StatUtilsTest() {
+
+    }
+
+    @Test
+    public void testCalculateAverage_NumberList(){
+        List<Integer> list = List.of(2, 4, 6, 8, 10);
+
+        Double result = statUtils.calculateAverage(list);
+
+        Assertions.assertEquals(6, result);
+    }
+
+    @Test
+    public void testCalculateAverage_EmptyList(){
+        List<Integer> list = List.of();
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            statUtils.calculateAverage(list);
+        });
+    }
+}
