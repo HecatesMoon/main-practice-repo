867dd0a694aa8c2d0c76e6235dda390a91ee3be0
JoacoOrbit <88943020+HecatesMoon@users.noreply.github.com>
2026-08-15 20:34:02 -0400

    test: add tests for RutUtils

---
+++ java/testing/testing-exercises1/src/test/java/com/hecatesmoon/testingexercises1/utils/RutUtilsTest.java
@@ -0,0 +1,83 @@
+package com.hecatesmoon.testingexercises1.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import com.hecatesmoon.testingexercises1.exceptions.InvalidRutException;
+
+public class RutUtilsTest {
+    
+    private final RutUtils rutUtils = new RutUtils();
+
+    public RutUtilsTest(){}
+
+    @ParameterizedTest
+    @CsvSource({
+    "5126663-3",
+    "5811892-3",
+    "8714763-0",
+    "24763195-k"
+    })
+    public void testRutValidator_ValidRuts(String rut){
+        boolean result = rutUtils.rutValidator(rut);
+
+        Assertions.assertEquals(true, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+    "5126663-k",
+    "5811892-0",
+    "8714763-2",
+    })
+    public void testRutValidator_InvalidVerificationDigit(String rut){
+        boolean result = rutUtils.rutValidator(rut);
+
+        Assertions.assertEquals(false, result);
+    }
+
+    @Test
+    public void testRutValidator_ValidRutWithUpperCaseK(){
+        String rut = "24763195-K";
+
+        boolean result = rutUtils.rutValidator(rut);
+
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void testRutValidator_EmptyString(){
+        String rut = "";
+
+        Assertions.assertThrows(InvalidRutException.class, () -> {
+            rutUtils.rutValidator(rut);
+        });
+    }
+
+    @Test
+    public void testRUtValidator_InvalidRutWithDotsBetween(){
+        String rut = "8.714.763-0";
+
+        Assertions.assertThrows(InvalidRutException.class, () -> {
+            rutUtils.rutValidator(rut);
+        });
+    }
+    @Test
+    public void testRUtValidator_InvalidRutWithLettersBetween(){
+        String rut = "87ia763-0";
+
+        Assertions.assertThrows(InvalidRutException.class, () -> {
+            rutUtils.rutValidator(rut);
+        });
+    }
+    @Test
+    public void testRUtValidator_InvalidRutWithoutHypen(){
+        String rut = "87147630";
+
+        Assertions.assertThrows(InvalidRutException.class, () -> {
+            rutUtils.rutValidator(rut);
+        });
+    }
+}
