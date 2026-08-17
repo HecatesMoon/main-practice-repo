7b05235460f688458dd25cb2e6c8aedc78b6f56c
JoacoOrbit <88943020+HecatesMoon@users.noreply.github.com>
2026-08-16 23:14:04 -0400

    test: add tests foradd and edit product methods

--- java/testing/testing-exercises1/src/test/java/com/hecatesmoon/testingexercises1/inventory/SimpleInventoryTest.java
+++ java/testing/testing-exercises1/src/test/java/com/hecatesmoon/testingexercises1/inventory/SimpleInventoryTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.hecatesmoon.testingexercises1.exceptions.ProductAlreadyExistsException;
+import com.hecatesmoon.testingexercises1.exceptions.ProductNotFoundException;
+
 public class SimpleInventoryTest {
 
     private SimpleInventory inventory;
@@ -25,7 +28,44 @@ public class SimpleInventoryTest {
         inventory.addProduct(product, quantity);
         Map<String, Integer> newInventory = inventory.getInventory();
 
-        Assertions.assertTrue((newInventory.containsKey(product) && 
-                                newInventory.get(product).equals(quantity)));
+        Assertions.assertTrue(newInventory.containsKey(product));
+        Assertions.assertTrue(newInventory.get(product).equals(quantity));
+    }
+
+    @Test
+    public void testAddProdduct_AlreadyExistingProduct(){
+        String product = "apple pies";
+        int quantity = 4;
+        inventory.addProduct(product, quantity);
+
+        Assertions.assertThrows(ProductAlreadyExistsException.class, () -> {
+            inventory.addProduct(product, quantity);
+        });
+    }
+
+    @Test
+    public void testEditProductQty_ExistingProduct(){
+        String product = "apple juice";
+        int quantity = 12;
+        int newValue = 6;
+        inventory.addProduct(product, quantity);
+
+        inventory.editProductQty(product, newValue);
+
+        Assertions.assertTrue(inventory.getInventory().containsKey(product));
+        Assertions.assertTrue(inventory.getInventory().get(product).equals(newValue));
+    }
+
+    @Test
+    public void testEditProductQty_NonExistingProduct(){
+        String product = "apple juice";
+        int quantity = 12;
+        String product2 = "strawberry cake";
+        int quantity2 = 3;
+        inventory.addProduct(product, quantity);
+
+        Assertions.assertThrows(ProductNotFoundException.class, () -> {
+            inventory.editProductQty(product2, quantity2);
+            });
     }
 }
