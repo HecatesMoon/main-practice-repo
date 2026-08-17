039f6c9959f4865b354134d8ae30ca7976093324
JoacoOrbit <88943020+HecatesMoon@users.noreply.github.com>
2026-08-17 00:07:12 -0400

    feat: add tests for reduceStock ad lowStockProducts

--- java/testing/testing-exercises1/src/test/java/com/hecatesmoon/testingexercises1/inventory/SimpleInventoryTest.java
+++ java/testing/testing-exercises1/src/test/java/com/hecatesmoon/testingexercises1/inventory/SimpleInventoryTest.java
@@ -1,11 +1,14 @@
 package com.hecatesmoon.testingexercises1.inventory;
 
+import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.hecatesmoon.testingexercises1.exceptions.InsufficientStockException;
+import com.hecatesmoon.testingexercises1.exceptions.InventoryIsEmptyException;
 import com.hecatesmoon.testingexercises1.exceptions.ProductAlreadyExistsException;
 import com.hecatesmoon.testingexercises1.exceptions.ProductNotFoundException;
 
@@ -67,5 +70,74 @@ public class SimpleInventoryTest {
         Assertions.assertThrows(ProductNotFoundException.class, () -> {
             inventory.editProductQty(product2, quantity2);
             });
+    }
+
+    @Test
+    public void testReduceStock_PositiveNumber(){
+        String product = "apple juice";
+        int quantity = 12;
+        inventory.addProduct(product, quantity);
+
+        inventory.reduceStock(product, 2);
+
+        Assertions.assertEquals(10, inventory.getInventory().get(product));
+    }
+
+    @Test
+    public void testReduceStock_NegativeNumber(){
+        String product = "apple juice";
+        int quantity = 12;
+        inventory.addProduct(product, quantity);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            inventory.reduceStock(product, -2);
+        });
+    }
+
+    @Test
+    public void testReduceStock_ReducingMoreThanActualStock(){
+        String product = "apple juice";
+        int quantity = 12;
+        inventory.addProduct(product, quantity);
+
+        Assertions.assertThrows(InsufficientStockException.class, () -> {
+            inventory.reduceStock(product, 15);
+        });
+    }
+
+    @Test
+    public void testReduceStock_ProductDoesNotExist(){
+        String product = "apple juice";
+        int quantity = 12;
+        inventory.addProduct(product, quantity);
+        String product2 = "bread";
+        int quantity2 = 20;
+
+        Assertions.assertThrows(ProductNotFoundException.class, () -> {
+            inventory.reduceStock(product2, quantity2);
+        });
+    }
+
+    @Test
+    public void testLowStockProducts_SimpleList(){
+        inventory.addProduct("apple pie", 12);
+        inventory.addProduct("chocolate bar", 16);
+        inventory.addProduct("milkshake", 7);
+        inventory.addProduct("ice cream", 8);
+        inventory.addProduct("melon pan", 10);
+
+        List<String> result = inventory.lowStockProducts(10);
+        List<String> expected = List.of("milkshake", "ice cream", "melon pan");
+
+        Assertions.assertIterableEquals(expected, result);
+    }
+
+    @Test
+    public void testLowStockProducts_EmptyList(){
+       
+        Assertions.assertThrows(InventoryIsEmptyException.class, () -> {
+            inventory.lowStockProducts(4);
+        });
+
     }
 }
