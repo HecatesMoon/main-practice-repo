cefff8a478d84f5d2bdfe9d014f65645615412d3
JoacoOrbit <88943020+HecatesMoon@users.noreply.github.com>
2026-08-18 11:38:06 -0400

    chore: initialize new testing prokect

---
+++ java/testing/testing-mockito-exercise/src/main/java/com/hecatesmoon/testingmockitoexercise/App.java
@@ -0,0 +1,10 @@
+package com.hecatesmoon.testingmockitoexercise;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
