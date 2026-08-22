fcaf65e07a8e8bb6f029af7455ea511033312da5
JoacoOrbit <88943020+HecatesMoon@users.noreply.github.com>
2026-08-22 16:04:40 -0400

    chore: initialize spring project

---
+++ java/testing/testing-mockmvc-exercise/src/main/java/com/hecatesmoon/testingmockmvcexercise/TestingMockmvcExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.hecatesmoon.testingmockmvcexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingMockmvcExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingMockmvcExerciseApplication.class, args);
+	}
+
+}
---
+++ java/testing/testing-mockmvc-exercise/src/test/java/com/hecatesmoon/testingmockmvcexercise/TestingMockmvcExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hecatesmoon.testingmockmvcexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingMockmvcExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
